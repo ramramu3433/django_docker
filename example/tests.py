0ba337245bec8ee658fc1d8f7a55fbbd489d5b6a
rakesh635 <rakesh635@gmail.com>
2016-09-09 11:37:52 +0000

    Second  commit

---
+++ example/tests.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+
+from example.models import example
+# Create your tests here.
+class exampletest(TestCase):
+      def createexample(self,fname="janakiraman",fphone="9176724389"):
+          return example.objects.create(name=fname,phone=fphone)
+      def test_example(self):
+          r=self.createexample()
+          self.assertTrue(isinstance(r,example))
+          
+	  
+          
+         
---
+++ example/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from example.models import example
+from django.conf import  settings 
+import calendar
+
+# Create your views here.
+ 
+def index(request):
+    t=[]
+    for i in range(1,12):
+        t.append(calendar.HTMLCalendar().formatmonth(2016,i))
+    return HttpResponse(t)
+    
+def janakiraman(request): 
+    return render(request,'hello.html',{'name':'janakiraman'})
+
+def form(request):
+    return render(request,'form.html')
+
+def thank(request):
+    message = request.GET['name']
+    phonen   = request.GET['phone']
+    b= example(name=message,phone=phonen)
+    b.save()
+    return render(request,'hello.html',{'name':message})
+
+def table(request):
+    i=example.objects.all() 
+    return render(request,'table.html',{'queryset':i})
+
+def delete(request):
+    t=example.objects.all().delete()
+    return HttpResponse(request,"All records are deleted") 
