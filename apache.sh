6df03ccd5f7a6cfe5a3529713e93256df58ff767
Janakiraman <ramramu.jr@gmail.com>
2016-11-29 16:21:38 +0530

    Update apache.sh
    testing phase db perm

--- apache.sh
+++ apache.sh
@@ -16,7 +16,8 @@ Alias /static /var/www/static
 #this is for give write permission to user for localdb
 
 chown -R $APACHE_RUN_USER:$APACHE_RUN_GROUP  /var/www/*
-
-chmod -R 754 /var/www/*
+chown -R $APACHE_RUN_USER:$APACHE_RUN_GROUP  /var/*
+chmod -R 755 *.sqlite3
+chmod  -R 755 /var/www/*
 
 
