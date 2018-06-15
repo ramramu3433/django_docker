efa15292a4263bb9ac4ceaa7a65958ae8f1526e4
Janakiraman <ramramu.jr@gmail.com>
2018-06-15 16:27:38 +0530

    Update views.py

--- example/views.py
+++ example/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from example.models import Example
-from django.conf import  settings 
+from example.models import example
+from django.conf import  settings
 import calendar
 
 # Create your views here.
- 
+
 def index(request):
     t=[]
     for i in range(1,12):
         t.append(calendar.HTMLCalendar().formatmonth(2017,i))
     return HttpResponse(t)
-    
-def janakiraman(request): 
+
+def janakiraman(request):
     return render(request,'hello.html',{'name':'janakiraman'})
 
 def form(request):
@@ -21,14 +21,14 @@ def form(request):
 def thank(request):
     message = request.GET['name']
     phonen   = request.GET['phone']
-    b= Example(name=message,phone=phonen)
+    b= example(name=message,phone=phonen)
     b.save()
     return render(request,'hello.html',{'name':message})
 
 def table(request):
-    i=Example.objects.all() 
+    i=example.objects.all()
     return render(request,'table.html',{'queryset':i})
 
 def delete(request):
-    t=Example.objects.all().delete()
-    return HttpResponse(request,"All records are deleted") 
+    t=example.objects.all().delete()
+    return HttpResponse(request,"All records are deleted")
